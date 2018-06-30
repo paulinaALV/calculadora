014159767836c11f250181ad12ccf18c90064f25
paulinaALV <velezpaulina139@gmail.com>
2018-06-30 12:11:48 -0500

    hola

---
+++ calculadora.js
@@ -0,0 +1,12 @@
+
+
+var radio = prompt("cual es el  radio");
+var altura = prompt("cual es la altura"); 
+var volumen_cilindro = Math.PI* radio*radio* altura;
+
+console.log ("el volumen del cilindro es" + volumen_cilindro)
+alert ("el volumen del cilindro es" + volumen_cilindro)
+
+
+
+
